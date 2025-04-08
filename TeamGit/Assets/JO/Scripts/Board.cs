9c0b2abde357b3beb2856f304cff8802f2a30375
JayJeongJW <p1fixer2@gmail.com>
2025-04-08 17:14:55 +0900

    보드 스크립트 추가

--- TeamGit/Assets/JO/Scripts/Board.cs
+++ TeamGit/Assets/JO/Scripts/Board.cs
@@ -1,32 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
-
-
 
 public class Board : MonoBehaviour
 {
-
-    public Transform cards;
     public GameObject card;
 
     // Start is called before the first frame update
     void Start()
     {
-        int[] arr = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
-        arr = arr.OrderBy(x => Random.Range(0f, 7f)).ToArray();
+        int[] arr = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, };
+        arr = arr.OrderBy(x => Random.Range(0f, 10f)).ToArray();
 
-        for (int i = 0; i < 16; i++)
+
+        for (int i = 0; i < 12; i++)
         {
             GameObject go = Instantiate(card, this.transform);
 
             float x = (i % 4) * 1.4f - 2.1f;
-            float y = (i / 4) * 1.4f - 3.0f;
+            float y = (i / 4) * 1.4f - 2.3f;
 
             go.transform.position = new Vector2(x, y);
-            //go.GetComponent<Card>().Setting(arr[i]);
+            go.GetComponent<Card>().idx = arr[i];
         }
     }
 
