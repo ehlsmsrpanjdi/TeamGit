3f0e8d3f0fcdf601b59de2e7f212bb6ffbafe710
성민 유 <usm9401@gmail.com>
2025-04-08 16:09:25 +0900

    유성민 : 매니저 생성

--- TeamGit/Assets/SM/GameManager.cs
+++ TeamGit/Assets/SM/GameManager.cs
@@ -2,17 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance = null;
+
+    public Card firstCard;
+    public Card secondCard;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(GameManager.Instance == null)
+        {
+            GameManager.Instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+
+    public bool CardCheck()
+    {
+        //if(firstCard)
+        return false;
     }
 }
